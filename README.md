161fc4b35dbef7d304c6becc547c404815e99a21
Jay Payne <letterj@gmail.com>
2017-07-09 09:02:34 -0500

    Wrap up skill for first attempt at submission

--- README.md
+++ README.md
@@ -4,21 +4,30 @@ This is an DBA Jokebook implemented as an Alexa skill.
 ### Todo
 
  * Write more jokes
- * Create github repo
- * Update intents
+ * ~~Rework Intents~~
+ * ~~Rebuild the model~~
+ * ~~create Icons~~
+ 
 
-
-### Tools
+### Tools and References
 
  * https://aws.amazon.com/lambda/
  * https://developer.amazon.com/
+ 
 
 
 ### Details
 
 Key | Values
 --- | -----
-Name | Database Jokes
-Invocation Name | Database Jokes
+Name | Database Comic
+Invocation Name | database jokes
+
+
+### Icons
+
+Small: 108 x 108 pixel PNG (can include transparency) or JPG file that represents this skill. Displays in the Alexa App.	
+
+Large: 512 x 512 pixel PNG (can include transparency) or JPG file that represents this skill. Displays in the Alexa App on larger screens.
 
 
--- data/jokes.js
+++ data/jokes.js
@@ -1,34 +1,27 @@
-
-
- 
-// A DBA retired and opened his dream bar “ROLLBACK” but every time he tried to run a credit card charge it canceled.
-// Cartoon of a two guys standing together on looking embarrassed the other looking down at broken pieces saying, “Tell me, you didn’t drop it”
-
+// Jokes and speech syntax 
 
 // <say-as interpret-as="spell-out">SQL</say-as>.
 // <say-as interpret-as="spell-out">DBA</say-as>, 
 
-
 var data = [
-    "A <say-as interpret-as="spell-out">DBA</say-as>, goes into a bar and walks up to two tables and says may I join you?",
-    "A <say-as interpret-as="spell-out">DBA</say-as>, is great in a relationship because they know how to commit.",
-    "A <say-as interpret-as="spell-out">DBA</say-as>, goes to see a no <say-as interpret-as="spell-out">SQL</say-as>, comic perform but does get any of the jokes until he is driving home after the show.",
-    "A <say-as interpret-as="spell-out">DBA</say-as>, quit going to a no <say-as interpret-as="spell-out">SQL</say-as>, bar because he couldn't find a table.",
-    "Dinner was a disaster because the <say-as interpret-as="spell-out">DBA</say-as>, tried to insert the food into the kitchen table.",
-    "With everyone coming over for Thanksgiving, grandma hired a <say-as interpret-as="spell-out">DBA</say-as>, to plan for the correct table spaces.",
-    "Why is a <say-as interpret-as="spell-out">DBA</say-as>, so fun at parties?  At the end of the night they can sort the keys.",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "", 
-    "",
-    "",  
+    'A <say-as interpret-as="spell-out">DBA</say-as>, goes into a bar and walks up to two tables and says may I join you?',
+    'A <say-as interpret-as="spell-out">DBA</say-as>, is great in a relationship because they know how to commit.',
+    'A <say-as interpret-as="spell-out">DBA</say-as>, goes to see a no <say-as interpret-as="spell-out">SQL</say-as> comic perform, but does not get any of the jokes until he is driving home after the show.',
+    'A <say-as interpret-as="spell-out">DBA</say-as>, quit going to the no <say-as interpret-as="spell-out">SQL</say-as> bar, because he could not find a table.',
+    'Dinner was a disaster because the <say-as interpret-as="spell-out">DBA</say-as>, tried to insert the food into the kitchen table.',
+    'With everyone coming over for Thanksgiving, grandma hired a <say-as interpret-as="spell-out">DBA</say-as>, to plan for the correct use of table space.',
+    'Why is a <say-as interpret-as="spell-out">DBA</say-as>, so fun at parties?  Because at the end of the night they can sort the keys.',
+    'A <say-as interpret-as="spell-out">DBA</say-as>, retired and opened his dream bar the ROLLBACK but every time he tried to run a credit card charge it canceled.',
+    'The <say-as interpret-as="spell-out">DBA</say-as>, was fired from his job at the furniture store because every time he was asked to clean up he would drop a table.',
+    'How does a <say-as interpret-as="spell-out">DBA</say-as>, change a light bulb?  Before he changes the bulb he has to replicate the fixture.',
+    'In an Emergency measure to save the no <say-as interpret-as="spell-out">SQL</say-as> chemical company the <say-as interpret-as="spell-out">DBA</say-as> poured acid on all their transactions.',
+    'the band Cap Theorm opened their first concert with the song two out of three is not bad.',
+    'When the database dropped acid it started seeing pretty colors and things that were not really there.',
+    'At the Christmas Party, the <say-as interpret-as="spell-out">DBA</say-as>, saw the full moon and turned into a were clause.',
+    'Little Johnny Drop Table, left a wake of destruction at every school he enrolled in.',
+    'The <say-as interpret-as="spell-out">DBA</say-as> Astronomer spent his life on one project. Select count star.',
+    'Being familiar with all types of joins, the <say-as interpret-as="spell-out">DBA</say-as> Minister was able to marry people in any type of relationship, polygamist, traditional or communal',
+    'Being previously trained to grant access made the <say-as interpret-as="spell-out">DBA</say-as>  the perfect candidate for the door man job.',
+    'Taking a job as a life guard at a Connection pool is not a great way to start database training.', 
+    'Roy Rogers was invited to the Western Database Conference and asked to give a keynote on how he made movies using Triggers.'
 ];
